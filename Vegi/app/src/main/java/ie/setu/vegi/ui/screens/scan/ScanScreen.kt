424400dd5a4aa52bd77ce1a67b6a3a5ad4a9cc9c
SineadCleary <sineadcleary9@gmail.com>
2026-04-16 12:34:57 +0100

    Log barcode

--- Vegi/app/src/main/java/ie/setu/vegi/ui/screens/scan/ScanScreen.kt
+++ Vegi/app/src/main/java/ie/setu/vegi/ui/screens/scan/ScanScreen.kt
@@ -3,13 +3,14 @@ package ie.setu.vegi.ui.screens.scan
 import androidx.compose.runtime.Composable
 import ie.setu.vegi.ui.components.camera.CameraPermissionScreen
 import ie.setu.vegi.ui.components.camera.CameraPreview
+import timber.log.Timber
 
 @Composable
 fun ScanScreen() {
     CameraPermissionScreen {
         CameraPreview(
             onBarcodeScanned = { barcode ->
-                // handle barcode
+                Timber.i(barcode)
             }
         )
     }
